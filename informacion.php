488dbf7ed5d4e4d7cee9086c66c5816c21f19045
paulaBS1012 <128522439+paulaBS1012@users.noreply.github.com>
2023-07-27 15:20:13 -0500

    Create informacion.php

---
+++ informacion.php
@@ -0,0 +1,13 @@
+<?php
+$Identificacion = $_POST['Identificacion'];
+$NomApellidos = $_POST['NomApellidos'];
+$Cargo= $_POST['Cargo'];
+$Salario= $_POST['Salario'];
+echo "LA IDENTIFICACION DEL EMPLEADO ES: ".$Identificacion;
+echo "<br>";
+echo "LOS NOMBRES DEL EMPLEADO SON: ".$NomApellidos;
+echo "<br>";
+echo "EL CARGO DEL EMPLEADO ES: ".$Cargo;
+echo "<br>";
+echo "EL SALARIO DEL EMPLEADO ES: ".$Salario;
+?>
